907d2c185c21116b5820ae383d6455efda1432b1
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2022-06-15 23:36:09 +0900

    記事を更新するためのルーティングとアクションを実装 #11

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,12 @@ class PostsController < ApplicationController
     render json: post
   end
 
+  def update
+    post = Post.find(params[:id])
+
+    post.update(post_params);
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body, :user_id)
